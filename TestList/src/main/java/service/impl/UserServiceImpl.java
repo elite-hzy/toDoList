46e26e12fb64f602547b0c2dc283bb46ff856474
eliteHzy <892621684@qq.com>
2021-11-11 16:13:03 +0800

    新添加过滤器，必须先登录再能登上界面

--- TestList/src/main/java/service/impl/UserServiceImpl.java
+++ TestList/src/main/java/service/impl/UserServiceImpl.java
@@ -9,11 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import service.UserService;
 
+import javax.servlet.http.HttpSession;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
+
+
+    @Autowired
+    private HttpSession session;
 
     //登录界面
     @Override
@@ -26,6 +32,13 @@ public class UserServiceImpl implements UserService {
         if (byUser == null) {
             return new ResultInfo(false,"用户名或密码错误");
         }
+        //这里的userSession1是存进session的
+        User userSession = new User();
+        userSession.setUser_name(user_name);
+        System.out.println("存进session的数据"+userSession);
+        //在session里放对象
+        session.setAttribute("user",userSession);
+
         return new ResultInfo(true,"正确");
     }
 }
